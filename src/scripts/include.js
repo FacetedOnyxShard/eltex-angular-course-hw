10e046432bc14d9327b86d7b1e8d5fc0ecd43b23
hikaruvi <daniilrybkin32@gmail.com>
2026-03-14 15:50:32 +0700

    feat: make a signgle header and footer

---
+++ src/scripts/include.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function () {
+  function loadComponent(url, placeholderId) {
+    return fetch(url)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`Ошибка загрузки ${url}: ${response.status}`);
+        }
+        return response.text();
+      })
+      .then((html) => {
+        document.getElementById(placeholderId).innerHTML = html;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  const COMPONENT_PATH = "/src/pages/components/";
+
+  Promise.all([
+    loadComponent(COMPONENT_PATH + "header.html", "header-placeholder"),
+    loadComponent(COMPONENT_PATH + "footer.html", "footer-placeholder"),
+  ]).catch((e) => console.error(e));
+});
