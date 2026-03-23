de50e3bc6ad03b3ba1774f1f7276deff834e0a8a
hikaruvi <daniilrybkin32@gmail.com>
2026-03-23 20:12:05 +0700

    feat(in process): add dynamic posts

--- src/scripts/blog-script.js
+++ src/scripts/blog-script.js
@@ -1,3 +1,28 @@
+// util functions
+function formatDate(date) {
+  const months = [
+    "января",
+    "февраля",
+    "марта",
+    "апреля",
+    "мая",
+    "июня",
+    "июля",
+    "августа",
+    "сентября",
+    "октября",
+    "ноября",
+    "декабря",
+  ];
+
+  const day = date.getDate();
+  const month = months[date.getMonth()];
+  const year = date.getFullYear();
+
+  return `${day} ${month} ${year}`;
+}
+
+// logic functions
 function addElementsToBlog() {
   const container = document.getElementById("small-cards-placeholder");
   const template = document.getElementById("small-card-template");
@@ -10,18 +35,49 @@ function addElementsToBlog() {
 
 addElementsToBlog();
 
-function initDialogLogic() {
+function commonDialogEvents(dialog, openButton, closeButton) {
+  dialog.addEventListener("click", (e) => {
+    if (e.target === dialog) {
+      dialog.classList.remove("show-dialog");
+    }
+  });
+  openButton.addEventListener("click", () => {
+    dialog.classList.add("show-dialog");
+  });
+  closeButton.addEventListener("click", () => {
+    dialog.classList.remove("show-dialog");
+  });
+}
+
+function getArticlesFromLocalStorage() {
+  localStorage.getItem("articles").split(",") ?? [];
+}
+
+function addArticle() {
+  const form = document.getElementById("form--bright");
+
+  form.onsubmit = (e) => {
+    e.preventDefault();
+
+    const newArticle = {
+      id: crypto.randomUUID(),
+      image: "../empty-picture.png",
+      title: document.getElementById("form-title-input").value,
+      content: document.getElementById("form-article-content").value,
+      publicationDate: new Date(),
+    };
+
+    localStorage.setItem("articles", JSON.stringify(newArticle));
+    form.reset();
+  };
+}
+
+function initAddPostDialogLogic() {
   const dialog = document.getElementById("dialog-container");
   const openButton = document.getElementById("open-button-for-add-post");
   const closeButton = document.getElementById("close-button-for-dialog");
 
-  openButton.addEventListener("click", () => {
-    dialog.style.display = "flex";
-  });
-  closeButton.addEventListener("click", () => {
-    dialog.style.display = "none";
-  });
-
+  commonDialogEvents(dialog, openButton, closeButton);
   // кнопки на форме
   const addButton = document.getElementById("dialog-form-add-post-button");
   const cancelButton = document.getElementById("dialog-form-cancel-button");
@@ -29,8 +85,10 @@ function initDialogLogic() {
   if (!dialog || !openButton || !closeButton || !addButton || !cancelButton)
     return false;
 
+  addArticle();
+
   cancelButton.addEventListener("click", () => {
-    dialog.style.display = "none";
+    dialog.classList.remove("show-dialog");
   });
 
   return true;
@@ -41,27 +99,7 @@ function initStatsDialogLogic() {
   const openButton = document.getElementById("open-button-for-stats");
   const closeButton = document.getElementById("close-button-for-stats-dialog");
 
-  openButton.addEventListener("click", () => {
-    dialog.style.display = "flex";
-  });
-  closeButton.addEventListener("click", () => {
-    dialog.style.display = "none";
-  });
-
-  // кнопки на форме
-  const addButton = document.getElementById(
-    "stats-dialog-form-add-post-button",
-  );
-  const cancelButton = document.getElementById(
-    "stats-dialog-form-cancel-button",
-  );
-
-  if (!dialog || !openButton || !closeButton || !addButton || !cancelButton)
-    return false;
-
-  cancelButton.addEventListener("click", () => {
-    dialog.style.display = "none";
-  });
+  commonDialogEvents(dialog, openButton, closeButton);
 
   return true;
 }
@@ -75,7 +113,7 @@ export function waitForDialogElements() {
     const statsOpenButton = document.getElementById("open-button-for-add-post");
 
     if (dialog && openButton && statsDialog && statsOpenButton) {
-      if (initDialogLogic() && initStatsDialogLogic()) {
+      if (initAddPostDialogLogic() && initStatsDialogLogic()) {
         obs.disconnect();
       }
     }
@@ -90,3 +128,23 @@ export function waitForDialogElements() {
 document.addEventListener("DOMContentLoaded", function () {
   waitForDialogElements();
 });
+
+function calculatePostsCount() {
+  const postContainer = document.getElementById("posts-section");
+  const articles = postContainer.querySelectorAll("article");
+
+  const metricContainer = document.getElementById("posts-count");
+  metricContainer.innerHTML = `
+    <h4 class="metric-section__title">Всего статей:</h4>
+    <p class="metric-section__metric">${articles.length}</p>
+  `;
+}
+
+calculatePostsCount();
+
+function workWithDynamicArticles() {
+  let articles = JSON.parse(localStorage.getItem("articles")) ?? [];
+  console.log(articles);
+}
+
+workWithDynamicArticles();
