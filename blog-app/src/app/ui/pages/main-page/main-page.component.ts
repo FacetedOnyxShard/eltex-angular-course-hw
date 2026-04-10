00089337f294299aa0bc464fe7e6b81739575679
hikaruvi <daniilrybkin32@gmail.com>
2026-04-10 18:33:05 +0700

    feat: add main-content component

--- blog-app/src/app/ui/pages/main-page/main-page.component.ts
+++ blog-app/src/app/ui/pages/main-page/main-page.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { FooterFormComponent } from '../../components/footer-form/footer-form.component';
 import { FooterComponent } from '../../components/footer/footer.component';
+import { MainContentComponent } from '../../components/main-content/main-content.component';
 
 @Component({
   selector: 'app-main-page',
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, MainContentComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './styles/main-page.component.scss',
 })
