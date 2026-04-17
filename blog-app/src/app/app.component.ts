0544f72279aabe424b1590054dd755512d3cf934
hikaruvi <daniilrybkin32@gmail.com>
2026-04-17 11:30:11 +0700

    move app-header, app-footer in app-component

--- blog-app/src/app/app.component.ts
+++ blog-app/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MainPageComponent } from './ui/pages/main-page/main-page.component';
+import { HeaderComponent } from './ui/components/header/header.component';
+import { FooterComponent } from './ui/components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MainPageComponent],
+  imports: [RouterOutlet, MainPageComponent, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'blog-app';
