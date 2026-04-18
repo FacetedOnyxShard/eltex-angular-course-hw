1207f427cc94ce1b492c1f314d4b8911cc94bc94
hikaruvi <daniilrybkin32@gmail.com>
2026-04-18 09:25:07 +0700

    feat: add routing

--- blog-app/src/app/ui/components/header/header.component.ts
+++ blog-app/src/app/ui/components/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterModule } from '@angular/router';
+import { MainPageComponent } from '../../pages/main-page/main-page.component';
+import { BlogPageComponent } from '../../pages/blog-page/blog-page.component';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive, RouterModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent {}
