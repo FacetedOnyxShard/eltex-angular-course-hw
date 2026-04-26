fe3c8dd5d4f79aa3453ee251d8a82ada383a67af
hikaruvi <daniilrybkin32@gmail.com>
2026-04-26 19:41:41 +0700

    fix: route on browse button and gap in main page

--- blog-app/src/app/ui/components/recently-posts/recently-posts.component.ts
+++ blog-app/src/app/ui/components/recently-posts/recently-posts.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-recently-posts',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './recently-posts.component.html',
   styleUrl: './recently-posts.component.scss',
 })
-export class RecentlyPostsComponent {
-
-}
+export class RecentlyPostsComponent {}
